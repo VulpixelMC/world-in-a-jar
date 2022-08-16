af4c5a5e7782183cac9166641b4788586e17314e
Kneelawk <kneelawk@gmail.com>
2022-08-16 02:51:55 -0700

    Add JarPlacer stub

---
+++ src/main/kotlin/dev/cursedmc/wij/api/generator/JarPlacer.kt
@@ -0,0 +1,8 @@
+package dev.cursedmc.wij.api.generator
+
+/**
+ * Does the calculations for figuring out where to place Jars in the Jar Dimension.
+ */
+object JarPlacer {
+	// TODO: actually implement this
+}
