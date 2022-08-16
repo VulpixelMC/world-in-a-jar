263b1b2f3c0f9792812e337caa44ac8862af3c9c
Kneelawk <kneelawk@gmail.com>
2022-08-16 02:43:05 -0700

    Make sure the same mod-id is used everywhere

--- src/main/kotlin/dev/cursedmc/wij/api/block/entity/BlockEntityTypes.kt
+++ src/main/kotlin/dev/cursedmc/wij/api/block/entity/BlockEntityTypes.kt
@@ -1,16 +1,16 @@
 package dev.cursedmc.wij.api.block.entity
 
 import dev.cursedmc.wij.api.Initializable
+import dev.cursedmc.wij.api.WIJConstants.id
 import dev.cursedmc.wij.api.block.Blocks
 import net.minecraft.block.entity.BlockEntityType
-import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 import org.quiltmc.qsl.block.entity.api.QuiltBlockEntityTypeBuilder
 
 object BlockEntityTypes : Initializable {
 	val WORLD_JAR: BlockEntityType<WorldJarBlockEntity> = Registry.register(
 		Registry.BLOCK_ENTITY_TYPE,
-		Identifier("worldinajar", "world_jar"),
+		id("world_jar"),
 		QuiltBlockEntityTypeBuilder.create(::WorldJarBlockEntity, Blocks.WORLD_JAR).build(),
 	)
 }
--- src/main/kotlin/dev/cursedmc/wij/api/item/group/ItemGroups.kt
+++ src/main/kotlin/dev/cursedmc/wij/api/item/group/ItemGroups.kt
@@ -1,14 +1,14 @@
 package dev.cursedmc.wij.api.item.group
 
 import dev.cursedmc.wij.api.Initializable
+import dev.cursedmc.wij.api.WIJConstants.id
 import dev.cursedmc.wij.api.block.Blocks
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
-import net.minecraft.util.Identifier
 import org.quiltmc.qsl.item.group.api.QuiltItemGroup
 
 object ItemGroups : Initializable {
-	val WORLD_JAR: ItemGroup = QuiltItemGroup.builder(Identifier("worldinajar", "world_jar"))
+	val WORLD_JAR: ItemGroup = QuiltItemGroup.builder(id("world_jar"))
 		.icon { return@icon ItemStack(Blocks.WORLD_JAR_ITEM) }
 		.build()
 }
