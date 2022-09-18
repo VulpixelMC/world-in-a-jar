0f7cda0e5b96e51f9de2ee57a8b9d2e73e51a645
TehcJS <tehcdev@gmail.com>
2022-09-17 21:51:29 -0400

    license change + forgot to push update source earlier (0.1.7)

--- src/main/kotlin/dev/cursedmc/wij/impl/WIJConstants.kt
+++ src/main/kotlin/dev/cursedmc/wij/impl/WIJConstants.kt
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022 CursedMC. All rights reserved.
+ *
+ * World In A Jar is common software: you can redistribute it and/or modify it under the terms of the Commons Protection License as published by the Revolutionary Technical Committee.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Commons Protection License for more details.
+ */
 package dev.cursedmc.wij.impl
 
 import net.minecraft.util.Identifier
