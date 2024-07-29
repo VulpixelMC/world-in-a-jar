41da837082601173f34b4e528f326ac51ca42393
Sylv <contact@sylv.gay>
2024-07-29 11:31:54 -0400

    feat: dropping bark

---
+++ src/main/java/gay/sylv/wij/mixin/AxeItemMixin.java
@@ -0,0 +1,60 @@
+package gay.sylv.wij.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import gay.sylv.wij.api.block.BarkType;
+import gay.sylv.wij.impl.item.Items;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Mixin(AxeItem.class)
+public final class AxeItemMixin {
+	@WrapOperation(
+			method = "useOn",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/item/AxeItem;evaluateNewBlockState(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/block/state/BlockState;)Ljava/util/Optional;"
+			)
+	)
+	private Optional<BlockState> onStrip(AxeItem instance, Level level, BlockPos pos, @Nullable Player player, BlockState state, Operation<Optional<BlockState>> original) {
+		Optional<BlockState> newState = original.call(instance, level, pos, player, state);
+		
+		if (newState.isPresent() && level instanceof ServerLevel serverLevel && player != null) {
+			BarkType.getTypes().stream()
+					.filter(type -> state.is(BuiltInRegistries.BLOCK.get(ResourceLocation.fromNamespaceAndPath(type.id().getNamespace(), type.id().getPath().replaceFirst("block/", "")))))
+					.forEach(type -> {
+						LootParams lootParams = new LootParams.Builder(serverLevel)
+								.withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos))
+								.withParameter(LootContextParams.TOOL, player.getMainHandItem())
+								.withParameter(LootContextParams.THIS_ENTITY, player)
+								.withParameter(LootContextParams.BLOCK_STATE, state)
+								.withLuck(player.getLuck())
+								.create(LootContextParamSets.BLOCK);
+						LootContext lootContext = new LootContext.Builder(lootParams).create(Optional.empty());
+						Block.popResource(level, player.blockPosition(), new ItemStack(Objects.requireNonNull(Items.BARK.get(type)), UniformGenerator.between(1.0F, 4.0F).getInt(lootContext)));
+					});
+		}
+		
+		return newState;
+	}
+}
