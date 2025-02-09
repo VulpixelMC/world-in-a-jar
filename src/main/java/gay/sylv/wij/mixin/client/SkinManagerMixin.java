28aeddce8f643b4398fa4090160085e06f4c3630
sylv256 <contact@sylv.gay>
2025-02-08 20:22:27 -0500

    chore: optimize imports

--- src/main/java/gay/sylv/wij/mixin/client/SkinManagerMixin.java
+++ src/main/java/gay/sylv/wij/mixin/client/SkinManagerMixin.java
@@ -4,14 +4,12 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.PlayerInfo;
-import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.resources.SkinManager;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import java.util.List;
 import java.util.Objects;
 
 @Mixin(SkinManager.class)
